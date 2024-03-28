6d0f524a9120d6bf497bccea8234e3591a2b6fc1
2001-SUHEB <syedsha.suheb2020@vitstudent.ac.in>
2024-03-28 15:57:44 +0530

    completed reduce task

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 //1.pop()-->removes and returns element from last
 
-let arr=[1,2,3,4,5,6,7]
+// let arr=[1,2,3,4,5,6,7]
 // let removedElement=arr.pop()
 // console.log(arr);
 // console.log(removedElement);
@@ -21,7 +21,126 @@ let arr=[1,2,3,4,5,6,7]
 // console.log(arr);
 // console.log(rm);
 
-//4.
+//4.unshift()-->opposite of push
+// let len=arr.unshift(99,88,77)
+// console.log(arr);
+// console.log(len);
+
+//====================================================================================
+
+//------------converting string to array------------------------------
+//-----------------------split()-----------------------
+
+// let str ="suheb"
+// let a=str.split("")
+// console.log(a);
+
+//--------------looping through elements of an array------------
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element);
+    
+// }
+
+
+//=====sample task - party queue===========================================
+
+// let que=["Bob","Alice","Charlie"]
+// que.push("David","Emily")
+// console.log(que);
+// que.shift()
+// let second_person = que.shift()
+// console.log(que);
+// console.log(second_person);
+// que.unshift("Richard")
+// console.log(que);
+
+//==============MAP============================================================
+//maps every element of an array and forms a new array
+// const num = [1,2,3,4,5,6]
+
+// const squarenum=num.map((element)=>{
+//     return element*element
+// })
+// console.log(squarenum);
+
+//======================TASK ON MAP=========================================
+
+// let celcius=[0,-40,100]
+
+// let farh=celcius.map((e)=>{
+//     let f=  (e * 9/5) + 32
+//     let c= e
+//     return [`c:${c},f:${f}`]
+// })
+// console.log(farh);
+
+//=======================ARRAY METHOD============================
+
+//1.find()---->finds first occurence of something in an array(does not modifes the array)
+
+// let num=[1,2,3,4,5,6]
+
+// let even=num.find((e)=>{
+//     return e%2==0
+// })
+
+// console.log(even);
+
+
+//2.filter()--->filters and finds all occurence-----------------
+
+// let num=[1,2,3,4,5,6]
+
+// let eve=num.filter((e)=>{
+//     return e%2==0
+// })
+
+// console.log(eve);
+
+//================================TASK ON FIND AND FILTER
+
+// let num=[1,2,3,4,5,6]
+
+// let all=num.filter((e)=>{
+//     return e%3==0
+// })
+// let oneNum=num.find((e)=>{
+//     return e%3==0
+// })
+// console.log(all);
+// console.log(oneNum);
+
+//=====================================================
+//3.reduce()===>reduces into one element
+
+// let arr=[1,2,3,4,5,6]
+
+// let sum=arr.reduce((h1,h2)=>{
+//     return h1+h2+0
+// })
+// let pro=arr.reduce((h1,h2)=>{
+//     return h1*h2
+// },1)
+
+// console.log(sum);
+// console.log(pro);
+//================================TASK ON REDUCE==================================
+
+// let arr=[1,0,2,3,0,2]
+
+// let nonZero=arr.reduce((h1,h2)=>{
+//     if(h2!=0){
+//         return h1*h2
+//     }
+//     else{
+//         return h1
+//     }
+// },1)
+
+console.log(nonZero);
+
 
 
 
